4c53e5b806704dc5b562fc833e64b1132287f87e
kula <kula@live.com>
2021-11-16 20:04:17 +0800

    feat: match erc1155

--- libs/ethereum/src/ethereum.service.ts
+++ libs/ethereum/src/ethereum.service.ts
@@ -14,22 +14,20 @@ export class EthereumService {
     const daiAddress = 'dai.tokens.ethers.eth';
 
     const abi = [
-      'function name() view returns (string)',
-      'function symbol() view returns (string)',
-
-      'function balanceOf(address) view returns (uint)',
-
-      'function transfer(address to, uint amount)',
-      // An event triggered whenever anyone transfers to someone else
-      'event Transfer(address indexed from, address indexed to, uint amount)',
+      'event TransferSingle(address indexed _operator, address indexed _from, address indexed _to, uint256 _id, uint256 _amount)',
+      'event TransferBatch(address indexed _operator, address indexed _from, address indexed _to, uint256[] _ids, uint256[] _amounts)',
+      'event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved)',
+      'function safeTransferFrom(address _from, address _to, uint256 _id, uint256 _amount, bytes calldata _data) external',
+      'function safeBatchTransferFrom(address _from, address _to, uint256[] calldata _ids, uint256[] calldata _amounts, bytes calldata _data) external',
+      'function balanceOf(address _owner, uint256 _id) external view returns (uint256)',
+      'function balanceOfBatch(address[] calldata _owners, uint256[] calldata _ids) external view returns (uint256[] memory)',
+      'function setApprovalForAll(address _operator, bool _approved) external',
+      'function isApprovedForAll(address _owner, address _operator) external view returns (bool isOperator)',
     ];
-    const contract = new ethers.Contract(
-      daiAddress,
-      abi,
-      this.provider.getSigner(0),
-    );
-    contract.on('Transfer', (to, amount, from) => {
-      console.log(to, amount, from);
+    const contract = new ethers.Contract(daiAddress, abi, this.provider);
+    contract.on('TransferSingle', (from, to, amount, event) => {
+      console.log(event);
+      console.log('---------------------------------------');
     });
     return block;
   }
